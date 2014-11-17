01325461a2e268eed80e8249cb000e31847d3277
Aspire Software <anant.shah@aspiresoftware.in>
2014-11-17 09:55:48 +0530

    Changes in comment into sql files

--- DB/FIN_54_2014_11_17.sql
+++ DB/FIN_54_2014_11_17.sql
@@ -1,14 +1,14 @@
 
-//Insert Education detail into code table
+# Insert Education detail into code table
 INSERT INTO `mifostenant-default`.`m_code` (`code_name`, `is_system_defined`) VALUES ('Education', '1');
-//Insert Education values into code_value table
+# Insert Education values into code_value table
 INSERT INTO `mifostenant-default`.`m_code_value` (`code_id`, `code_value`, `order_position`) VALUES ('18', 'N/A', '1');
 INSERT INTO `mifostenant-default`.`m_code_value` (`code_id`, `code_value`, `order_position`) VALUES ('18', 'Bachelors', '2');
 INSERT INTO `mifostenant-default`.`m_code_value` (`code_id`, `code_value`, `order_position`) VALUES ('18', 'Diploma', '3');
-//Insert Poverty detail into code table
+# Insert Poverty detail into code table
 INSERT INTO `mifostenant-default`.`m_code` (`code_name`, `is_system_defined`) VALUES ('Poverty', '1');
-//Insert Povarty values code_value table
+# Insert Povarty values code_value table
 INSERT INTO `mifostenant-default`.`m_code_value` (`code_id`, `code_value`, `order_position`) VALUES ('19', '0-10000', '1');
 INSERT INTO `mifostenant-default`.`m_code_value` (`code_id`, `code_value`, `order_position`) VALUES ('19', '10001-20000', '2');
-//Alter table m_client to add column education_cv_id and poverty_cv_id table
+# Alter table m_client to add column education_cv_id and poverty_cv_id table
 ALTER TABLE `mifostenant-default`.`m_client` ADD COLUMN `education_cv_id` INT(11) NULL DEFAULT NULL  AFTER `client_classification_cv_id` , ADD COLUMN `poverty_cv_id` INT(11) NULL DEFAULT NULL  AFTER `education_cv_id` ;
