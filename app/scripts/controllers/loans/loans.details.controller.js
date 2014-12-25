949f7ad803cda7eeeaf0f05aebbde00db4a4ea7f
Alex Litvinenko <limit85@gmail.com>
2014-12-25 17:34:01 +0200

    Fin-138 Added content for Collateral loan details tab

--- app/scripts/controllers/loans/loans.details.controller.js
+++ app/scripts/controllers/loans/loans.details.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-angular.module('angularjsApp').controller('LoansDetailsCtrl', function($route, REST_URL, LoanService, $timeout, $scope) {
+angular.module('angularjsApp').controller('LoansDetailsCtrl', function($route, REST_URL, LoanService, $timeout, $scope, dialogs) {
   console.log('LoansDetailsCtrl');
   $scope.clientId = $route.current.params.clientId;
   $scope.loanId = $route.current.params.loanId;
@@ -13,7 +13,7 @@ angular.module('angularjsApp').controller('LoansDetailsCtrl', function($route, R
     {name: 'repayment', view: 'views/loans/details/loans.details.repayment.html', title: 'Repayment Schedule', active: false, disabled: false},
     {name: 'transaction', view: 'views/loans/details/loans.details.transaction.html', title: 'Transaction History', active: false, disabled: false},
     {name: 'charges', view: 'views/loans/details/loans.details.charges.html', title: 'Charges', active: false, disabled: false},
-    {name: 'collateral', view: 'views/loans/details/loans.details.collateral.html', title: 'Collateral', active: false, disabled: true},
+    {name: 'collateral', view: 'views/loans/details/loans.details.collateral.html', title: 'Collateral', active: false, disabled: false},
     {name: 'guarantor', view: 'views/loans/details/loans.details.guarantor.html', title: 'Guarantor Info', active: false, disabled: true},
     {name: 'notes', view: 'views/loans/details/loans.details.notes.html', title: 'Notes', active: false, disabled: true}
   ];
@@ -75,9 +75,114 @@ angular.module('angularjsApp').controller('LoansDetailsCtrl', function($route, R
     $scope.chargesTab = {};
     $scope.chargesTab.loading = true;
     LoanService.getData(REST_URL.LOANS_CREATE + '/' + $scope.loanId + '/charges').then(function(result) {
-      console.log(result.data);
       $scope.chargesTab.charges = result.data;
       $scope.chargesTab.loading = false;
     });
   }
+
+
+  $scope.openCollateralDialog = function(collateral) {
+    console.log(collateral);
+    var dialog = dialogs.create('/views/loans/details/loans.details.collateral.dialog.html', 'LoanDeatilsCollateralDialog', {collateral: collateral, loan: $scope.loanDetails}, {size: 'md', keyboard: true, backdrop: true});
+    dialog.result.then(function(result) {
+      console.log(result);
+    });
+  };
+  $scope.removeCollateral = function(collateral) {
+    var msg = 'You are about to remove Collateral <strong>' + collateral.type.name + '</strong>';
+    var dialog = dialogs.create('/views/custom-confirm.html', 'CustomConfirmController', {msg: msg}, {size: 'sm', keyboard: true, backdrop: true});
+    dialog.result.then(function(result) {
+      if (result) {
+        var index = $scope.loanDetails.collateral.indexOf(collateral);
+        LoanService.removeLoan(REST_URL.LOANS_CREATE + '/' + $scope.loanDetails.id + '/collaterals/' + collateral.id).then(function() {
+          $scope.type = 'alert-success';
+          $scope.message = 'Collateral removed successfully.';
+          $scope.errors = [];
+          if (index >= -1) {
+            $scope.loanDetails.collateral.splice(index, 1);
+          } else {
+            LoanService.getData(REST_URL.LOANS_CREATE + '/' + $scope.loanDetails.id + '/collaterals').then(function(result) {
+              $scope.loan.collateral = result.data;
+            });
+          }
+        }, function(result) {
+          $scope.message = 'Cant remove collateral:' + result.data.defaultUserMessage;
+          $scope.type = 'error';
+          $scope.errors = result.data.errors;
+        });
+      }
+    });
+
+  };
+
+});
+
+
+angular.module('angularjsApp').controller('LoanDeatilsCollateralDialog', function($route, REST_URL, LoanService, $timeout, $scope, $modalInstance, dialogs, data) {
+  $scope.collateral = {};
+  $scope.loan = data.loan;
+  LoanService.getData();
+  $scope.cancel = function() {
+    $modalInstance.dismiss(false);
+  };
+
+  LoanService.getData(REST_URL.LOANS_CREATE + '/' + $scope.loan.id + '/collaterals/template').then(function(result) {
+    $scope.data = result.data;
+    if (data.collateral) {
+      $timeout(function() {
+        $scope.collateral.collateralTypeId = data.collateral.type.id;
+        $scope.collateral.description = data.collateral.description;
+        $scope.collateral.value = data.collateral.value;
+        $scope.collateralId = data.collateral.id;
+      });
+    }
+
+  });
+  function updateLoanCollaterals() {
+    LoanService.getData(REST_URL.LOANS_CREATE + '/' + $scope.loan.id + '/collaterals').then(function(result) {
+      $scope.loan.collateral = result.data;
+    });
+  }
+  updateLoanCollaterals();
+
+  $scope.filterCollaterals = function(items) {
+    return _.filter(items, function(item) {
+      try {
+        return parseInt($scope.collateral.collateralTypeId) === parseInt(item.id) || !_.find($scope.loan.collateral, function(collateral) {
+          return collateral.type.id === item.id;
+        });
+      } catch (e) {
+        console.log('Cant parse Collateral ID');
+      }
+    });
+  };
+
+  $scope.saveCollateral = function() {
+    if (!$scope.loanDetailsFormCollateral.$valid) {
+      $scope.type = 'error';
+      $scope.message = 'Highlighted fields are required';
+      $scope.errors = [];
+      return;
+    }
+    var data = angular.copy($scope.collateral);
+    data.locale = 'en';
+    function handleSuccess() {
+      $scope.type = 'alert-success';
+      $scope.message = 'Collateral added successfully.';
+      $scope.errors = [];
+      $scope.collateral = {};
+      updateLoanCollaterals();
+      $scope.$modalInstance.dismiss(true);
+    }
+    function handleFail(result) {
+      $scope.message = 'Cant add collateral:' + result.data.defaultUserMessage;
+      $scope.type = 'error';
+      $scope.errors = result.data.errors;
+    }
+    if ($scope.collateralId) {
+      LoanService.updateLoan(REST_URL.LOANS_CREATE + '/' + $scope.loan.id + '/collaterals/' + $scope.collateralId, data).then(handleSuccess, handleFail);
+    } else {
+      LoanService.saveLoan(REST_URL.LOANS_CREATE + '/' + $scope.loan.id + '/collaterals', data).then(handleSuccess, handleFail);
+    }
+  };
 });
