bd6558df215bffe3707aad98872d3aded7427cd8
architnaskar <architnaskar@gmail.com>
2014-12-26 22:23:40 +0530

    Fin - 147 - Completed

--- app/scripts/controllers/holidays/holidayController.js
+++ app/scripts/controllers/holidays/holidayController.js
@@ -57,54 +57,49 @@ angular.module('angularjsApp').controller('HolidayController', function($route, 
 // Create Holiday
 angular.module('angularjsApp').controller('CreateHolidayController', function($location, $scope, REST_URL, HolidayService, $timeout, Utility) {
   console.log('CreateHolidayController');
-  $scope.isLoading = false;
   $scope.offices = [];
   $scope.formHoliday = {};
   var holidayOfficeIdArray = [];
   $scope.holidayApplyToOffice = function (node) {
-      console.log('out node.selectedCheckBox > '+ node.selectedCheckBox);
-      console.log(node.selectedCheckBox === true);
-      console.log(node.selectedCheckBox == true);
-      var a = 'true';
-      console.log(node.selectedCheckBox == a);
-      console.log(node.selectedCheckBox === a);
-      if (node.selectedCheckBox === 'true') {
-          console.log('in node.selectedCheckBox > '+node.selectedCheckBox);
-          node.selectedCheckBox = 'true';
-          recurHolidayApplyToOffice(node);
-          holidayOfficeIdArray = _.uniq(holidayOfficeIdArray);
-      } else {
-          node.selectedCheckBox = 'false';
-          recurRemoveHolidayAppliedOOffice(node);
-
-      }
+    console.log('holidayApplyToOffice');
+    if (node.selectedCheckBox === 'true') {
+        node.selectedCheckBox = 'true';
+        recurHolidayApplyToOffice(node);
+        holidayOfficeIdArray = _.uniq(holidayOfficeIdArray);
+    } else {
+        node.selectedCheckBox = 'false';
+        recurRemoveHolidayAppliedOOffice(node);
+    }
   };
   function recurHolidayApplyToOffice(node) {
-      node.selectedCheckBox = 'true';
-      holidayOfficeIdArray.push(node.id);
-      if (node.children.length > 0) {
-          for (var i = 0; i < node.children.length; i++) {
-              node.children[i].selectedCheckBox = 'true';
-              holidayOfficeIdArray.push(node.children[i].id);
-              if (node.children[i].children.length > 0) {
-                  recurHolidayApplyToOffice(node.children[i]);
-              }
-          }
-      }
+    console.log('recurHolidayApplyToOffice');
+    node.selectedCheckBox = 'true';
+    holidayOfficeIdArray.push(node.id);
+    if (node.children.length > 0) {
+        for (var i = 0; i < node.children.length; i++) {
+            node.children[i].selectedCheckBox = 'true';
+            holidayOfficeIdArray.push(node.children[i].id);
+            if (node.children[i].children.length > 0) {
+                recurHolidayApplyToOffice(node.children[i]);
+            }
+        }
+    }
   }
   function recurRemoveHolidayAppliedOOffice(node) {
-      holidayOfficeIdArray = _.without(holidayOfficeIdArray, node.id);
-      if (node.children.length > 0) {
-          for (var i = 0; i < node.children.length; i++) {
-              node.children[i].selectedCheckBox = 'false';
-              holidayOfficeIdArray = _.without(holidayOfficeIdArray, node.children[i].id);
-              if (node.children[i].children.length > 0) {
-                  recurRemoveHolidayAppliedOOffice(node.children[i]);
-              }
-          }
-      }
+    console.log('recurRemoveHolidayAppliedOOffice');
+    holidayOfficeIdArray = _.without(holidayOfficeIdArray, node.id);
+    if (node.children.length > 0) {
+        for (var i = 0; i < node.children.length; i++) {
+            node.children[i].selectedCheckBox = 'false';
+            holidayOfficeIdArray = _.without(holidayOfficeIdArray, node.children[i].id);
+            if (node.children[i].children.length > 0) {
+                recurRemoveHolidayAppliedOOffice(node.children[i]);
+            }
+        }
+    }
   }
   function buildTreeData(data) {
+    console.log('buildTreeData');
     var map = {}, item, result = {};
     for (var i = 0; i < data.length; i += 1) {
       item = data[i];
@@ -134,7 +129,6 @@ angular.module('angularjsApp').controller('CreateHolidayController', function($l
   };
   //Success callback
   var loadOfficesSuccess = function(result) {
-    $scope.isLoading = false;
     $scope.offices = result.data;
     try {      
       $scope.treedata = buildTreeData(result.data);
@@ -144,32 +138,26 @@ angular.module('angularjsApp').controller('CreateHolidayController', function($l
   };
   //failure callback
   var loadOfficesFail = function(result) {
-    $scope.isLoading = false;
     console.log('Error : Return from HolidayService service.' + result);
   };
-  var loadOffices = function getData() {
-    $scope.isLoading = true;
-    $timeout(
-      function() {
-        HolidayService.getData(REST_URL.OFFICE_LIST).then(loadOfficesSuccess, loadOfficesFail);
-      }, 2000);
-  };
-  loadOffices();
+  HolidayService.getData(REST_URL.OFFICE_LIST).then(loadOfficesSuccess, loadOfficesFail);  
   // Save holiday
   $scope.saveHoliday = function() {
-    var saveHolidaySuccess = function() {
+    console.log('saveHoliday');
+    var saveHolidaySuccess = function(result) {
+      console.log('saveHolidaySuccess');
       $scope.type = 'alert-success';
       $scope.message = 'Saved successfully';
       $scope.errors = [];
-      $location.url('/holidays');
+      $scope.activeHoliday(result.data);
     };
     var saveHolidayFail = function(result) {
+      console.log('saveHolidayFail');
       $scope.type = 'error';
       $scope.message = 'Holiday not saved: ' + result.data.defaultUserMessage;
       $scope.errors = result.data.errors;
       $('html, body').animate({scrollTop: 0}, 800);
     };
-    //TODO
     $scope.formHoliday.offices = [];
     for (var i in holidayOfficeIdArray) {
         var temp = {};
@@ -180,13 +168,13 @@ angular.module('angularjsApp').controller('CreateHolidayController', function($l
     $scope.formHoliday.dateFormat = 'dd/MM/yyyy';
     var date;
     if (typeof $scope.formHoliday.fromDate === 'object') {      
-      $scope.formHoliday.fromDate = Utility.dataFormat($scope.date.fromDate);
+      $scope.formHoliday.fromDate = Utility.dataFormat($scope.formHoliday.fromDate);
     }
     if (typeof $scope.formHoliday.toDate === 'object') {
-      $scope.formHoliday.toDate = Utility.dataFormat($scope.date.toDate);
+      $scope.formHoliday.toDate = Utility.dataFormat($scope.formHoliday.toDate);
     }
     if (typeof $scope.formHoliday.repaymentsRescheduledTo === 'object') {
-      $scope.formHoliday.repaymentsRescheduledTo = Utility.dataFormat($scope.date.repaymentsRescheduledTo);
+      $scope.formHoliday.repaymentsRescheduledTo = Utility.dataFormat($scope.formHoliday.repaymentsRescheduledTo);
     }
     var json = angular.toJson($scope.formHoliday);
     console.log('json > '+ json);
@@ -194,6 +182,7 @@ angular.module('angularjsApp').controller('CreateHolidayController', function($l
   };
   //Validate form and save
   $scope.validate = function() {
+    console.log('validate');
     $scope.type = '';
     $scope.message = '';
     $scope.errors = [];
@@ -205,6 +194,20 @@ angular.module('angularjsApp').controller('CreateHolidayController', function($l
       $scope.errors = [];
       $('html, body').animate({scrollTop: 0}, 800);
     }
+  };
+  //Active Holiday
+  $scope.activeHoliday = function(data){
+    console.log('activeHoliday');
+    var activeHolidaySuccess = function(result) {
+      console.log('Success : activeHolidaySuccess' + result);
+      $location.url('/holidays');
+    }
+    var activeHolidayFail = function(result) {
+      console.log('Failure : activeHolidayFail' + result);
+      $location.url('/holidays');
+    }
+    var url = REST_URL.HOLIDAYS + '/' + data.resourceId + '?command=activate';
+    HolidayService.saveHoliday(url).then(activeHolidaySuccess, activeHolidayFail);
   };
   //For date
   $scope.openedFromDate = false;
@@ -226,15 +229,16 @@ angular.module('angularjsApp').controller('CreateHolidayController', function($l
     $scope.openedRepaymentDate = true;
   };
 });
+
 // Edit Holiday
 angular.module('angularjsApp').controller('EditHolidayController', function($route, $location, $scope, REST_URL, HolidayService, $timeout, Utility) {
   console.log('EditHolidayController');
-  $scope.isLoading = false;
   $scope.formHoliday = {};
   $scope.date = {};
   $scope.id = $route.current.params.id;
   //Success callback
   var loadHolidaySuccess = function(result) {
+    console.log('loadHolidaySuccess');
     $scope.isLoading = false;    
     try {
       $scope.formHoliday = {
@@ -262,17 +266,9 @@ angular.module('angularjsApp').controller('EditHolidayController', function($rou
   };
   //failure callback
   var loadHolidayFail = function(result) {
-    $scope.isLoading = false;
     console.log('Error : Return from HolidayService service.' + result);
   };
-  var loadHoliday = function getData() {
-    $scope.isLoading = true;
-    $timeout(
-      function() {
-        HolidayService.getData(REST_URL.HOLIDAYS + '/' + $scope.id).then(loadHolidaySuccess, loadHolidayFail);
-      }, 2000);
-  };
-  loadHoliday();
+  HolidayService.getData(REST_URL.HOLIDAYS + '/' + $scope.id).then(loadHolidaySuccess, loadHolidayFail);  
   // Save holiday
   $scope.updateHoliday = function() {
     var saveHolidaySuccess = function() {
