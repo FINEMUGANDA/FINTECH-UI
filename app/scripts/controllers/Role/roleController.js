4975fadc534b91c8b6be4711c0d62d6d89dee963
Aleksandar Vidakovic <cheetah@monkeysintown.com>
2015-02-20 23:28:38 +0000

    Fix JsLint error

--- app/scripts/controllers/Role/roleController.js
+++ app/scripts/controllers/Role/roleController.js
@@ -22,10 +22,7 @@ angular.module('angularjsApp').controller('RoleController', function ($route, $s
     $scope.format = function(format) {
         var args = Array.prototype.slice.call(arguments, 1);
         return format.replace(/{(\d+)}/g, function(match, number) {
-            return typeof args[number] != 'undefined'
-                ? args[number]
-                : match
-                ;
+            return typeof args[number] !== 'undefined' ? args[number] : match;
         });
     };
 
