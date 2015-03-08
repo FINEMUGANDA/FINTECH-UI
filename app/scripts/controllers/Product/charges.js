52db3e713f58296ff436d4a48bf803b26f4cd87d
Aleksandar Vidakovic <cheetah@monkeysintown.com>
2015-03-08 23:04:59 +0000

    Disable non-sense validation rule when creating charge

--- app/scripts/controllers/Product/charges.js
+++ app/scripts/controllers/Product/charges.js
@@ -169,6 +169,13 @@ chargesController.controller('EditChargeCtrl', function($scope, $location, $time
         }
       }
     };
+    if ($route.current.params.id) {
+      ChargesService.updateCharge(REST_URL.RETRIVE_CHARGE_BY_ID + $route.current.params.id, angular.toJson($scope.chargeDetails)).then(updateChargeSuccess, updateChargeFail);
+    } else {
+      ChargesService.saveCharge(REST_URL.CREATE_CHARGE, angular.toJson($scope.chargeDetails)).then(updateChargeSuccess, updateChargeFail);
+    }
+    // TODO: find out why?!? see above, they commented the code out, but forgot it here
+    /**
     if (this.chargeDetails.chargeCalculationType !== '1' && $scope.chargeDetails.amount > 100) {
       $scope.type = 'error';
       $scope.message = 'Amount(%) must be or equal to 100';
@@ -182,5 +189,6 @@ chargesController.controller('EditChargeCtrl', function($scope, $location, $time
         ChargesService.saveCharge(REST_URL.CREATE_CHARGE, angular.toJson($scope.chargeDetails)).then(updateChargeSuccess, updateChargeFail);
       }
     }
+    */
   };
 });
