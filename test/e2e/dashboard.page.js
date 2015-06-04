1d228edb45635d8b240a1b860dd492828638a5e7
Denys Digtiar <duemir@gmail.com>
2015-06-04 20:36:38 +1000

    Navigation Bar is just shared as a singleton object.
    Inheritance looked too cumbersome. It doesn't seem to
     worth it.

--- test/e2e/dashboard.page.js
+++ test/e2e/dashboard.page.js
@@ -1,48 +1,37 @@
 'use strict';
 
-// Proof of concept: Sharing of the Header functionality via inheritance.
-// So far it looks like it might not worth it because of cumbersome way of replicating traditional inheritance in JS
-
-var util = require('util');
-
-var BasePage = require('./base.page.js');
 
 var DashboardPage = function () {
-    BasePage.call(this);
 };
 
 // instead of hardcoding URLs, page objects can have one attached to them
 DashboardPage.URL = '/dashboard';
 
-// In contrast to LoginPage, DashboardPage's prototype is not created using `Object.create`, but inherited from BasePage and than extended.
-util.inherits(DashboardPage, BasePage);
-
-// Define additional properties on the prototype inherited from 
-Object.defineProperties(DashboardPage.prototype, {
+DashboardPage.prototype = Object.create({}, {
     shortcuts: {
         value: Object.create({}, {
             newClient: {
                 value: function () {
                     return element(by.css('.shortcuts'))
-                           .element(by.linkText('New Client'))
+                        .element(by.linkText('New Client'));
                 }
             },
             newLoan: {
                 value: function () {
                     return element(by.css('.shortcuts'))
-                           .element(by.linkText('New Loan'))
+                        .element(by.linkText('New Loan'));
                 }
             },
             newRepayment: {
                 value: function () {
                     return element(by.css('.shortcuts'))
-                           .element(by.linkText('New Repayment'))
+                        .element(by.linkText('New Repayment'));
                 }
             },
             newNote: {
                 value: function () {
                     return element(by.css('.shortcuts'))
-                           .element(by.linkText('New Note'))
+                        .element(by.linkText('New Note'));
                 }
             }
         })
--- test/e2e/navbar.page.js
+++ test/e2e/navbar.page.js
@@ -1,13 +1,6 @@
 'use strict';
 
-// One possible way to abstract a header into a re-usable component (more of a traditional Object Oriented one).
-// "Abstract" Page Object wich declares all the header elements and from which every other Page Object inherits.
-// Inheritence can be ditched and this constructor function can be rewritten as a "singleton" object with all the same functions. 
-
-var BasePage = function () {
-};
-
-BasePage.prototype = Object.create({}, {
+module.exports = Object.create({}, {
     navMenu: {
         value: Object.create({}, {
             home: {
@@ -38,5 +31,3 @@ BasePage.prototype = Object.create({}, {
         })
     }
 });
-
-module.exports = BasePage;
